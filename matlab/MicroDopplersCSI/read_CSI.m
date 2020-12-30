0cb6fecca0971591816a37818f7b2919511f344b
ahmedag91 <ahmed.abdulgawad91@gmail.com>
2020-12-30 05:53:38 +0100

    docs: Update read_CSI.m file

--- matlab/MicroDopplersCSI/read_CSI.m
+++ matlab/MicroDopplersCSI/read_CSI.m
@@ -1,9 +1,9 @@
 function CTF = read_CSI()
-% This matlab function aims to read a file in .dat format which stores the
-% the measured CSI data. Then it returns the channel transfer function (CTF) as a cell array
-% This cell contains Ntx X Nrx matrices, where Ntx and Nrx denote the number of active transmit
-% and receive antenna, respectively. Each matrix corresponds to a CTF with dimensions Nf X Nt, where Nf % and Nt denote the number of frequency and time bins, respectively. Nf always has a value of 30,
-% whereas Nt is variant depending on the sampling rate dt and how many recording seconds.
+    % This matlab function aims to read a file in .dat format which stores the
+    % the measured CSI data. Then it returns the channel transfer function (CTF) as a cell array
+    % This cell contains Ntx X Nrx matrices, where Ntx and Nrx denote the number of active transmit
+    % and receive antenna, respectively. Each matrix corresponds to a CTF with dimensions Nf X Nt, where Nf % and Nt denote the number of frequency and time bins, respectively. Nf always has a value of 30,
+    % whereas Nt is variant depending on the sampling rate dt and how many recording seconds.
 
     [file_name,dr ] = uigetfile({'*.dat'});
     x = read_bf_file([dr file_name ]);
